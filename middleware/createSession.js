e623bcc2b4c3d57f801bebc758bdeb76353afabb
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-10-30 10:46:46 -0500

    prep for pulling from db schema

--- middleware/createSession.js
+++ middleware/createSession.js
@@ -5,19 +5,11 @@ const db = require("../database/db-config");
 const cookieParser = require("cookie-parser");
 
 const store = new KnexSessionStore({
-<<<<<<< HEAD
   tablename: "sessions",
   sidfieldname: "sid",
   knex: db,
-  clearInterval: 1000 * 60 * 60 * 24,
+  clearInterval: 1 * 24 * 60 * 60 * 1000,
   createtable: true
-=======
-    tablename: 'sessions',
-    sidfieldname: 'sid',
-    knex: db,
-    clearInterval: 1 * 24 * 60 * 60 * 1000,
-    createtable: true
->>>>>>> ff36fb242855c0343bd13ef88df42ca48fb59db3
 });
 
 const sessionConfig = {
