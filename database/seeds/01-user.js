770355dab10ab4bdcc483a301b36de5b97dc1993
Dave Vazquez <dmvaz.li@gmail.com>
2020-01-27 14:39:09 -0500

    encrypts passwords in need data

--- database/seeds/01-user.js
+++ database/seeds/01-user.js
@@ -5,63 +5,63 @@ exports.seed = function(knex) {
     {
       user_type: 'parent',
       username: 'parent',
-      password: 'password',
+      password,
       email: 'email1',
       name: 'name'
     },
     {
       user_type: 'admin',
       username: 'admin',
-      password: 'password',
+      password,
       email: 'email2',
       name: 'name'
     },
     {
       user_type: 'staff',
       username: 'staff',
-      password: 'password',
+      password,
       email: 'email3',
       name: 'name'
     },
     {
       user_type: 'parent',
       username: 'parent2',
-      password: 'password',
+      password,
       email: 'email4',
       name: 'name'
     },
     {
       user_type: 'admin',
       username: 'admin2',
-      password: 'password',
+      password,
       email: 'email5',
       name: 'name'
     },
     {
       user_type: 'staff',
       username: 'staff2',
-      password: 'password',
+      password,
       email: 'email6',
       name: 'name'
     },
     {
       user_type: 'parent',
       username: 'parent3',
-      password: 'password',
+      password,
       email: 'email7',
       name: 'name'
     },
     {
       user_type: 'admin',
       username: 'admin3',
-      password: 'password',
+      password,
       email: 'email8',
       name: 'name'
     },
     {
       user_type: 'staff',
       username: 'staff3',
-      password: 'password',
+      password,
       email: 'email9',
       name: 'name'
     }
