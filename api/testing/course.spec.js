ba158bf883b9e19c88e8752a848bac4fcffbad4b
Eric Richards <esholundr@yahoo.com>
2020-02-05 14:31:01 -0700

    got some tests to work

--- api/testing/course.spec.js
+++ api/testing/course.spec.js
@@ -1,41 +1,109 @@
-const server = require('../server');
+const server = require('../server.js');
+// const db = require('../../database/db-config.js');
+const request = require('supertest');
 
-const request = require('supertest')(server);
+const createObj = {
+	term_id: 1,
+	course_type_id: 1,
+	group_type_id: 1,
+	school_grade_id: 1,
+	level_id: 1,
+	section: 'A',
+	subsection: 1,
+	hourly_rate: 7.0,
+	course_schedule_id: 1,
+	room_id: 1,
+	start_time: '16:30:00',
+	end_time: '18:30:00',
+	teacher_id: 1,
+	notes: 'Notes',
+	status: 'active'
+};
 
-describe('server.js', function() {
-	describe('environment', function() {
-		it('should set environment to testing', function() {
-			expect(process.env.NODE_ENV).toBe('testing');
+const editObj = {
+	term_id: 1,
+	course_type_id: 1,
+	group_type_id: 1,
+	school_grade_id: 1,
+	level_id: 1,
+	section: 'A',
+	subsection: 1,
+	hourly_rate: 7.0,
+	course_schedule_id: 1,
+	room_id: 1,
+	start_time: '16:30:00',
+	end_time: '18:30:00',
+	teacher_id: 1,
+	notes: 'Notes',
+	status: 'active'
+};
+
+describe('server.js', () => {
+	describe('environment', () => {
+		it('should set environment to testing', () => {
+			expect(process.env.DB_ENV).toBe('testing');
 		});
 	});
 
-	describe('FIND /course', function() {
-		it('should return a 200 OK', function() {
+	describe('FIND /course', () => {
+		it('should return a 200 OK', () => {
 			return request(server)
-				.find('/course')
+				.get('/course')
 				.then(res => {
 					expect(res.status).toBe(200);
 				});
 		});
-		it('should return a JSON', function() {
+		it('should return a JSON', () => {
 			return request(server)
-				.find('/course')
+				.get('/course')
 				.then(res => {
 					expect(res.type).toMatch(/json/i);
 				});
 		});
 	});
-	describe('CREATE /course', function() {
-		it('should return a 200', function() {
+	describe('CREATE /course', () => {
+		it('should return a 200', () => {
 			return request(server)
-				.create('/course')
+				.post('/course', createObj)
 				.then(res => {
 					expect(res.status).toBe(200);
 				});
 		});
-		it('should return a JSON', function() {
+		it('should return a JSON', () => {
 			return request(server)
-				.create('/course')
+				.post('/course')
+				.then(res => {
+					expect(res.type).toMatch(/json/i);
+				});
+		});
+	});
+	describe('EDIT /course/:id', () => {
+		it('should return a 200', () => {
+			return request(server)
+				.put('/course/:id', editObj)
+				.then(res => {
+					expect(res.status).toBe(200);
+				});
+		});
+		it('should return a JSON', () => {
+			return request(server)
+				.put('/course/:id')
+				.then(res => {
+					expect(res.type).toMatch(/json/i);
+				});
+		});
+	});
+	describe('DELETE /course/:id', () => {
+		it('should return a 200', () => {
+			return request(server)
+				.delete('/course/:id')
+				.then(res => {
+					expect(res.status).toBe(200);
+				});
+		});
+		it('should return a JSON', () => {
+			return request(server)
+				.delete('/course/:id')
 				.then(res => {
 					expect(res.type).toMatch(/json/i);
 				});
