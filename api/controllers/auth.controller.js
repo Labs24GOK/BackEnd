b49da2331790e38827ffa7b87b0d653879ed5767
Dave Vazquez <dmvaz.li@gmail.com>
2020-02-06 22:33:19 -0500

    creates auth controller, middleware and router

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,35 +1,20 @@
 const bcrypt = require('bcrypt');
-const Auth = requre('../models/auth.model.js');
+const Auth = require('../models/auth.model.js');
 const { catchAsync } = require('../utils/catchAsync');
 
 const registerStudent = catchAsync(async (req, res) => {
   const hashedPassword = bcrypt.hashSync(
-    user,
-    password,
+    req.body.user.password,
     10
   );
-  req.body.user = {
-    username: user.username,
-    password: hashedPassword,
-    name: user.name || null,
-    email: user.email || null,
-    user_type: user.user_type
-  };
-  req.body.family = {
-    mother_name: family.mother_name,
-    father_name: family.father_name,
-    primary_telephone: family.primary_telephone,
-    secondary_telephone: family.secondary_telephone
-  };
 
+  req.body.user = {
+    ...req.body.user,
+    password: hashedPassword
+  };
   req.body.student = {
-    first_name: student.first_name,
-    additional_names: student.additional_names,
-    cpr: student.cpr,
-    email: student.email,
-    birthdate: student.birthdate,
-    registration_date: new Date(),
-    location_id: student.location_id
+    ...req.body.student,
+    registration_date: new Date()
   };
 
   const newStudent = Auth.registerStudent(
