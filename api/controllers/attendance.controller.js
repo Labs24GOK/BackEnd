31b6382d4451fb0d2d183cfce2540216e65d89d6
Eric Richards <esholundr@yahoo.com>
2020-02-19 16:12:10 -0700

    added endpoint for getAttendanceRecord

--- api/controllers/attendance.controller.js
+++ api/controllers/attendance.controller.js
@@ -2,10 +2,17 @@ const { catchAsync } = require('../utils/catchAsync');
 const Attendance = require('../models/attendance.model');
 
 const takeAttendance = catchAsync(async (req, res) => {
-  await Attendance.takeAttendance(req.body.meeting, req.body.students);
-  res.status(201).json({ message: 'Attendance submitted' });
+	await Attendance.takeAttendance(req.body.meeting, req.body.students);
+	res.status(201).json({ message: 'Attendance submitted' });
+});
+
+const getAttendanceRecord = catchAsync(async (req, res) => {
+	//get list of all teachers from staff model
+	const attendanceList = await Attendance.getAttendanceRecord(req.param.date);
+	res.status(200).json({ attendanceList });
 });
 
 module.exports = {
-  takeAttendance
+	takeAttendance,
+	getAttendanceRecord
 };
--- api/routes/attendance.routes.js
+++ api/routes/attendance.routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
-const { takeAttendance } = require('../controllers/attendance.controller');
+const {
+	takeAttendance,
+	getAttendanceRecord
+} = require('../controllers/attendance.controller');
 
 const router = express.Router();
 
 router.post('/attendance', takeAttendance);
+router.get('/attendance/:date', getAttendanceRecord);
 
 module.exports = router;
