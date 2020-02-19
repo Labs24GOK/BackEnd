14833cafb2d1194a9447afde6b8ae7a8377f2e9f
Dave Vazquez <dmvaz.li@gmail.com>
2020-02-19 18:39:00 -0500

    adds list of teachers to response body

--- api/controllers/attendance.controller.js
+++ api/controllers/attendance.controller.js
@@ -1,18 +1,28 @@
 const { catchAsync } = require('../utils/catchAsync');
 const Attendance = require('../models/attendance.model');
+const Staff = require('../models/staff.model');
 
 const takeAttendance = catchAsync(async (req, res) => {
-	await Attendance.takeAttendance(req.body.meeting, req.body.students);
-	res.status(201).json({ message: 'Attendance submitted' });
+  await Attendance.takeAttendance(
+    req.body.meeting,
+    req.body.students
+  );
+  res.status(201).json({ message: 'Attendance submitted' });
 });
 
 const getAttendanceRecord = catchAsync(async (req, res) => {
-	//get list of all teachers from staff model
-	const attendanceList = await Attendance.getAttendanceRecord(req.param.date);
-	res.status(200).json({ attendanceList });
+  // get list of all teachers from staff model
+  const teachers = await Staff.findAll();
+  const attendanceList = await Attendance.getAttendanceRecord(
+    req.params.date
+  );
+  res.status(200).json({
+    teachers,
+    attendanceList
+  });
 });
 
 module.exports = {
-	takeAttendance,
-	getAttendanceRecord
+  takeAttendance,
+  getAttendanceRecord
 };
--- api/models/attendance.model.js
+++ api/models/attendance.model.js
@@ -1,41 +1,41 @@
 const db = require('../../database/db-config');
 
 const takeAttendance = (meeting, students) => {
-	return db.transaction(trx => {
-		return db('meeting')
-			.transacting(trx)
-			.insert(meeting)
-			.returning('id')
-			.then(res => {
-				const [id] = res;
-				const attendance = students.map(student => {
-					student.meeting_id = id;
-				});
-				return db('attendance')
-					.transacting(trx)
-					.insert(attendance)
-					.returning('id');
-			})
-			.then(trx.commit)
-			.catch(trx.rollback);
-	});
+  return db.transaction(trx => {
+    return db('meeting')
+      .transacting(trx)
+      .insert(meeting)
+      .returning('id')
+      .then(res => {
+        const [id] = res;
+        const attendance = students.map(student => {
+          student.meeting_id = id;
+        });
+        return db('attendance')
+          .transacting(trx)
+          .insert(attendance)
+          .returning('id');
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  });
 };
 
 const getAttendanceRecord = date => {
-	return db('attendance as a')
-		.join('student as s', 's.id', 'a.student_id')
-		.join('meeting as m', 'm.id', 'a.meeting_id')
-		.where('m.meeting_date', '=', date)
-		.select([
-			'a.student_id',
-			's.first_name',
-			's.additional_names',
-			'a.attendance'
-		])
-		.orderBy('a.student_id', 'desc');
+  return db('attendance as a')
+    .join('student as s', 's.id', 'a.student_id')
+    .join('meeting as m', 'm.id', 'a.meeting_id')
+    .where('m.meeting_date', '=', date)
+    .select([
+      'a.student_id',
+      's.first_name',
+      's.additional_names',
+      'a.attendance'
+    ])
+    .orderBy('a.student_id', 'asc');
 };
 
 module.exports = {
-	takeAttendance,
-	getAttendanceRecord
+  takeAttendance,
+  getAttendanceRecord
 };
