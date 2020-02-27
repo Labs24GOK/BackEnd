e38aed2a918427c1e36b49deae7372c211d0bccb
Dave Vazquez <dmvaz.li@gmail.com>
2020-02-27 16:03:01 -0500

    updates seeds

--- database/seeds/08-term.js
+++ database/seeds/08-term.js
@@ -1,24 +1,22 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('term')
-    .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex('term').insert([
-        { name: 'Fall 2014' },
-        { name: 'Spring 2015' },
-        { name: 'Fall 2015' },
-        { name: 'Spring 2016' },
-        { name: 'Fall 2016' },
-        { name: 'Winter 2016' },
-        { name: 'Spring 2017' },
-        { name: 'Summer 2017' },
-        { name: 'Fall 2017' },
-        { name: 'Spring 2018' },
-        { name: 'Summer 2018' },
-        { name: 'Fall 2018' },
-        { name: 'Winter 2018' },
-        { name: 'Spring 2019' }
-      ]);
-    });
+  return knex('term').del().then(function() {
+    // Inserts seed entries
+    return knex('term').insert([
+      { name: 'Fall 2020' },
+      { name: 'Summer 2020' },
+      { name: 'Spring 2020' },
+      { name: 'Winter 2020' },
+      { name: 'Fall 2019' },
+      { name: 'Summer 2019' },
+      { name: 'Spring 2019' },
+      { name: 'Winter 2019' },
+      { name: 'Fall 2018' },
+      { name: 'Summer 2018' },
+      { name: 'Spring 2018' },
+      { name: 'Winter 2018' },
+      { name: 'Fall 2017' },
+      { name: 'Summer 2017' }
+    ]);
+  });
 };
