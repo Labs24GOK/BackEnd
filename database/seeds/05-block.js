b61867b3e4d77c1fa43c08fb1662c387d3328458
Richard Machado <rich92105@gmail.com>
2020-03-20 14:29:12 -0700

    moved routes out of server.js

--- database/seeds/05-block.js
+++ database/seeds/05-block.js
@@ -5,12 +5,18 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('block').insert([
-        { block_code: 363, neighborhood: 'Bilad alqadeem' },
-        { block_code: 431, neighborhood: 'Jabalt Hibshi' },
-        { block_code: 433, neighborhood: 'Jabalt Hibshi' },
-        { block_code: 435, neighborhood: 'Jabalt Hibshi' },
-        { block_code: 439, neighborhood: 'North Sehla' },
-        { block_code: 441, neighborhood: 'Al Hajar' }
+        { block_code: 363, 
+          neighborhood: 'Bilad alqadeem' },
+        { block_code: 431, 
+          neighborhood: 'Jabalt Hibshi' },
+        { block_code: 433, 
+          neighborhood: 'Jabalt Hibshi' },
+        { block_code: 435, 
+          neighborhood: 'Jabalt Hibshi' },
+        { block_code: 439, 
+          neighborhood: 'North Sehla' },
+        { block_code: 441, 
+          neighborhood: 'Al Hajar' }
       ]);
     });
 };
--- database/seeds/13-course_schedule.js
+++ database/seeds/13-course_schedule.js
@@ -5,12 +5,24 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('course_schedule').insert([
-        { short_description: 'Sat / Tue', long_description: 'Saturday / Tuesday', sql: '(1, 1)' },
-        { short_description: 'Sun / Wed', long_description: 'Sunday / Wednesday', sql: '(1, 2)' },
-        { short_description: 'Mon / Thu', long_description: 'Monday / Thursday', sql: '(1, 3)' },
-        { short_description: 'Sat / Wed', long_description: 'Saturday / Wednesday', sql: '(1, 4)' },
-        { short_description: 'Mon / Wed', long_description: 'Monday / Wednesday', sql: '(1, 5)' },
-        { short_description: 'Sun / Tue / Wed', long_description: 'Sunday / Tuesday / Wenesday', sql: '(1, 6)' }
+        { short_description: 'Sat / Tue', 
+        long_description: 'Saturday / Tuesday', 
+        sql: '(1, 1)' },
+        { short_description: 'Sun / Wed', 
+        long_description: 'Sunday / Wednesday', 
+        sql: '(1, 2)' },
+        { short_description: 'Mon / Thu', 
+        long_description: 'Monday / Thursday', 
+        sql: '(1, 3)' },
+        { short_description: 'Sat / Wed', 
+        long_description: 'Saturday / Wednesday', 
+        sql: '(1, 4)' },
+        { short_description: 'Mon / Wed', 
+        long_description: 'Monday / Wednesday', 
+        sql: '(1, 5)' },
+        { short_description: 'Sun / Tue / Wed', 
+        long_description: 'Sunday / Tuesday / Wednesday', 
+        sql: '(1, 6)' }
       ]);
     });
 };
