1fab6ceca290dc9a69e458d8d221ced5b4d96d27
Richard Machado <richardmachado@MacBook-Pro.local>
2020-04-21 14:35:46 -0700

    add family route and remove bad seeds

---
+++ api/models/family.model.js
@@ -0,0 +1,14 @@
+const db = require('../../database/db-config');
+
+
+
+const findAllFamilies = () => {
+  return db('family')
+    .select(['user_id','mother_name', 'father_name'])
+   
+};
+
+module.exports = {
+ 
+  findAllFamilies
+};
