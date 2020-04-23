a1df172cac249b7d349acd4856e26352b6c1842e
Hiter Harris <hiterharris@comcast.net>
2020-04-22 20:16:26 -0400

    semantics

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,16 +1,18 @@
+// const router = require('express').Router();
 const { catchAsync } = require('../utils/catchAsync');
 
 const Users = require('../models/user.model');
-
 
 const findAllUsers = catchAsync(async (req, res, next) => {
   const users= await Users.findAll();
   return res.status(200).json(users);
 });
 
-const findUserById =  catchAsync(async (req, res, next) => {
+const findUserById = catchAsync(async (req, res, next) => {
     const users= await Users.findBy();
     return res.status(200).json(users);
   });
+
+
 
 module.exports = { findAllUsers, findUserById };
