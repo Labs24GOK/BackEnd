2d1bf9ee4aef53704e61d1893204c82fee8d03bf
Gremlin4544 <alve@mckethen.com>
2020-05-19 20:18:52 -0400

    clean up code on auth controller - no changes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,32 +1,32 @@
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
-const { catchAsync } = require('../utils/catchAsync');
+// const { catchAsync } = require('../utils/catchAsync');
 
-const Auth = require('../models/auth.model');
+// const Auth = require('../models/auth.model');
 
-const registerStudent = catchAsync(async (req, res) => {
-  const hashedPassword = bcrypt.hashSync(
-    req.body.user.password,
-    10
-  );
+// const registerStudent = catchAsync(async (req, res) => {
+//   const hashedPassword = bcrypt.hashSync(
+//     req.body.user.password,
+//     10
+//   );
 
-  req.body.user = {
-    ...req.body.user,
-    password: hashedPassword
-  };
-  req.body.student = {
-    ...req.body.student,
-    registration_date: new Date()
-  };
+//   req.body.user = {
+//     ...req.body.user,
+//     password: hashedPassword
+//   };
+//   req.body.student = {
+//     ...req.body.student,
+//     registration_date: new Date()
+//   };
 
-  const newStudent = Auth.registerStudent(
-    req.body.user,
-    req.body.family,
-    req.body.student
-  );
-  return res.status(201).json(newStudent);
-});
+//   const newStudent = Auth.registerStudent(
+//     req.body.user,
+//     req.body.family,
+//     req.body.student
+//   );
+//   return res.status(201).json(newStudent);
+// });
 
-module.exports = {
-  registerStudent
-};
+// module.exports = {
+//   registerStudent
+// };
