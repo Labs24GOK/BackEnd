c17d1adb90a308a124d9f181c0b9e71d0626edee
Gremlin4544 <alve@mckethen.com>
2020-05-19 20:26:36 -0400

    comment out all code from family routes

--- api/routes/family.routes.js
+++ api/routes/family.routes.js
@@ -1,13 +1,13 @@
-const express = require('express');
+// const express = require('express');
 
-const router = express.Router();
+// const router = express.Router();
 
-const {
-    findAllFamilies,
-    findFamilyById
-  } = require('../controllers/family.controller');
+// const {
+//     findAllFamilies,
+//     findFamilyById
+//   } = require('../controllers/family.controller');
 
-router.get('/families', findAllFamilies);
-router.get('/families/:UserId', findFamilyById);
+// router.get('/families', findAllFamilies);
+// router.get('/families/:UserId', findFamilyById);
 
-module.exports = router;
+// module.exports = router;
