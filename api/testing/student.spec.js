5031cfce2bc966ddd0ec56d84adb8c5530d3093e
Gremlin4544 <alve@mckethen.com>
2020-05-21 18:49:47 -0400

    Create test to add a student from user dashboard
    Co-authored-by: Brittani Luce <c0der.br1t@gmail.com>
    Co-authored-by: Jeremy McWilliams <j2macwilliams@gmail.com>

--- api/testing/student.spec.js
+++ api/testing/student.spec.js
@@ -20,7 +20,6 @@ describe('admin - student routes', () => {
             .post('/api/auth/login')
             .send({ username: 'admin', password: 'pass' })
             .then(res => {
-                console.log(res.body.token);
                 token = res.body.token;
                 done();
             })
@@ -38,122 +37,97 @@ describe('admin - student routes', () => {
 
 });
 
-// describe('user - student routes', () => {
-//     beforeAll((done) => {
-//         request(server)
-//             .post('/login')
-//             .send({ username: 'admin', password: 'pass' })
-//             .then(res => {
-//                 token = res.body.token;
-//                 done();
-//             })
-//     });
-//     // beforeEach(async() => {
-//     //     await db('student').truncate();
-//     // });
-//     // afterAll((done) => {
-//     //     // remove token to sign out
-//     // });
+describe('user - student routes', () => {
+    beforeEach((done) => {
+        request(server)
+            .post('/api/auth/login')
+            .send({ username: 'parent', password: 'pass' })
+            .then(res => {
+                token = res.body.token;
+                done();
+            })
+    });
 
-//     // valid request body
-//     const requestBody1 = {
-//         cpr: '1234567890',
-//         first_name: 'Student1',
-//         additional_names: 'Abe Lincoln',
-//         gender: 'M',
-//         // home_telephone: '11-225566',
-//         // mobile_telephone: '11-334455',
-//         // ADD IN WHATEVER 'phone' IS GOING TO BE
-//         email: 'student1@email.com',
-//         // preferred_contact_type_id: 1,
-//         birthdate: new Date(2015, 3, 15),
-//         school_name: 'Abba',
-//         school_grade_id: 1,
-//         // location_id: 1,
-//         address: '1234 address st block 2345',
-//         primary_emergency_contact_name: 'Genghis Kahn',
-//         primary_emergency_relationship: 'guardian',
-//         primary_emergency_phone: '11-334455',
-//         emergency_contact_name: 'Mary Kahn',
-//         emergency_relationship: 'guardian',
-//         emergency_phone: '11-556677',
-//         notes: '',
-//         no_call: false,
-//         delinquent: false,
-//         expelled: false,
-//         user_id: 20
-//     };
+    // valid request body
+    const requestBody1 = {
+        cpr: '8675309JENNY',
+        first_name: 'Student1',
+        additional_names: 'Abe Lincoln',
+        gender: 'M',
+        phone_number: '11-233455',
+        email: 'student1@email.com',
+        birthdate: new Date(2015, 3, 15),
+        school_name: 'Abba',
+        school_grade_id: 1,
+        address: '1234 address st block 2345',
+        primary_emergency_contact_name: 'Genghis Kahn',
+        primary_emergency_relationship: 'guardian',
+        primary_emergency_phone: '11-334455',
+        emergency_contact_name: 'Mary Kahn',
+        emergency_relationship: 'guardian',
+        emergency_phone: '11-556677',
+        notes: '',
+        user_id: 6
+    };
 
-//     // malformed request body
-//     const malformedRequestBody1 = {
-//         cpr: '',
-//         first_name: '',
-//         additional_names: 'Abe Lincoln',
-//         gender: 'M',
-//         // home_telephone: '11-225566',
-//         // mobile_telephone: '11-334455',
-//         // ADD IN WHATEVER 'phone' IS GOING TO BE
-//         email: 'student1@email.com',
-//         // preferred_contact_type_id: 1,
-//         birthdate: new Date(2015, 3, 15),
-//         school_name: 'Abba',
-//         school_grade_id: 1,
-//         // location_id: 1,
-//         address: '1234 address st block 2345',
-//         primary_emergency_contact_name: 'Genghis Kahn',
-//         primary_emergency_relationship: 'guardian',
-//         primary_emergency_phone: '11-334455',
-//         emergency_contact_name: 'Mary Kahn',
-//         emergency_relationship: 'guardian',
-//         emergency_phone: '11-556677',
-//         notes: '',
-//         no_call: false,
-//         delinquent: false,
-//         expelled: false,
-//         user_id: 20
-//     };
+    // malformed request body
+    const malformedRequestBody1 = {
+        cpr: '',
+        first_name: '',
+        additional_names: 'Abe Lincoln',
+        gender: 'M',
+        phone_number: '22-233455',
+        email: 'student1@email.com',
+        birthdate: new Date(2015, 3, 15),
+        school_name: 'Abba',
+        school_grade_id: 1,
+        address: '1234 address st block 2345',
+        primary_emergency_contact_name: 'Genghis Kahn',
+        primary_emergency_relationship: 'guardian',
+        primary_emergency_phone: '11-334455',
+        emergency_contact_name: 'Mary Kahn',
+        emergency_relationship: 'guardian',
+        emergency_phone: '11-556677',
+        notes: '',
+        user_id: 5
+    };
 
-//     // edited request body
-//     const requestBody2 = {
-//         cpr: '1234567890',
-//         first_name: 'Student1',
-//         additional_names: 'George Washington',
-//         gender: 'M',
-//         // home_telephone: '11-225566',
-//         // mobile_telephone: '11-334455',
-//         // ADD IN WHATEVER 'phone' IS GOING TO BE
-//         email: 'student1@email.com',
-//         // preferred_contact_type_id: 1,
-//         birthdate: new Date(2015, 3, 15),
-//         school_name: 'Abba',
-//         school_grade_id: 1,
-//         // location_id: 1,
-//         address: '1234 address st block 2345',
-//         primary_emergency_contact_name: 'Genghis Kahn',
-//         primary_emergency_relationship: 'guardian',
-//         primary_emergency_phone: '11-334455',
-//         emergency_contact_name: 'Mary Kahn',
-//         emergency_relationship: 'guardian',
-//         emergency_phone: '11-556677',
-//         notes: 'Previous Name: Abe Lincoln',
-//         no_call: false,
-//         delinquent: false,
-//         expelled: false,
-//         user_id: 20
-//     };
+    // edited request body
+    const requestBody2 = {
+        cpr: '8675309JENNY',
+        first_name: 'Student1',
+        additional_names: 'George Washington',
+        gender: 'M',
+        phone_number: '11-233455',
+        email: 'student1@email.com',
+        birthdate: new Date(2015, 3, 15),
+        school_name: 'Abba',
+        school_grade_id: 1,
+        address: '1234 address st block 2345',
+        primary_emergency_contact_name: 'Genghis Kahn',
+        primary_emergency_relationship: 'guardian',
+        primary_emergency_phone: '11-334455',
+        emergency_contact_name: 'Mary Kahn',
+        emergency_relationship: 'guardian',
+        emergency_phone: '11-556677',
+        notes: 'Previous Name: Abe Lincoln',
+        user_id: 6
+    };
 
-    // describe('POST /student', () => {
-    //     it('Should return a json object.', () => {
-    //         return request(server)
-    //             .post('/student')
-    //             .set('Authorization', token)
-    //             .send(requestBody1)
-    //             .then(res => {
-    //                 expect(res.type).toMatch(/json/i);
-    //             });
-    //     });
-    // });
-
+    describe('POST /student', () => {
+        it('Will fail with a 500 as it needs a unique cpr and there is a foreign key constraint', () => {
+            return request(server)
+                .post('/student')
+                .set({ 'Authorization': token })
+                .send(requestBody1)
+                .then(res => {
+                    // console.log(res.body);
+                    expect(res.status).toBe(500);
+                    // expect(res.type).toMatch(/json/i);
+                });
+        });
+    });
+});
     // describe('GET /student/:studentID', () => {
     //     it('Should return a ')
     // })
