f9291cb345202e07e84354368d1f5c2bb632b0d2
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-06-01 16:24:44 -0700

    remove username and shortname. Fix helper model calls. Co-authored-by: Rudy Goldhaber <rgoldhaber24@gmail.com>

--- api/middlewares/staff.middleware.js
+++ api/middlewares/staff.middleware.js
@@ -1,165 +1,159 @@
 const AppError = require('../utils/AppError');
 const { catchAsync } = require('../utils/catchAsync');
 const User = require('../models/user.model');
-
+const Model = require('../models/model.js');
 const Staff = require('../models/staff.model');
 
 const validateCreateStaff = catchAsync(async (req, res, next) => {
-  const {
-    // username,
-    password,
-    email,
-    name,
-    short_name,
-    cpr,
-    mobile_number,
-    accent,
-    gender,
-    birthdate,
-    teaching_rate,
-    admin,
-    active,
-  } = req.body;
-  if (
-    admin === null ||
-    admin === undefined ||
-    // !username ||
-    !email ||
-    !name ||
-    !password ||
-    !gender
-  ) {
-    next(new AppError('Wrong Body', 400));
-    return;
-  }
-  req.user = {
-    user_type: admin ? 'admin' : 'staff',
-    // username,
-    email,
-    name,
-    password,
-    short_name: short_name || null,
-  };
+	const {
+		password,
+		email,
+		name,
+		cpr,
+		mobile_number,
+		accent,
+		gender,
+		birthdate,
+		teaching_rate,
+		admin,
+		active,
+	} = req.body;
+	if (
+	  admin === null ||
+	  admin === undefined ||
+	  !email ||
+	  !name ||
+	  !password ||
+	  !gender
+	) {
+	  next(new AppError('Wrong Body', 400));
+	  return;
+	}
+	req.user = {
+		user_type: admin,
+		email,
+		name,
+		password,
+		
+	};
 
-  // CHECKS IF EMAIL USERNAME OR CPR IS IN USE
-  const userByEmail = await User.findBy('email', email);
-  // const userByUsername = await User.findBy('username', username);
+	// CHECKS IF EMAIL USERNAME IS IN USE
 
-  if (userByEmail) {
-    return next(new AppError('User with that email already exists', 401));
-  }
-  // if (userByUsername) {
-  //   return next(new AppError('User with that username already exists', 401));
-  // }
+	if (email) {
+		const userByEmail = await Model.findByEmail({email});
+		if (userByEmail) {
+			return next(new AppError('User with that email already exists', 401));
+		}
+	}
 
-  if (cpr) {
-    const staffByCPR = await Staff.findByCPR(cpr);
-    if (staffByCPR) {
-      return next(
-        new AppError('Staff member with that CPR already exists', 401)
-      );
-    }
-  }
+	if (cpr) {
+		const staffByCPR = await Staff.findByCPR(cpr);
+		if (staffByCPR) {
+			return next(
+				new AppError('Staff member with that CPR already exists', 401)
+			);
+		}
+	}
 
-  req.staff = {
-    teaching_rate,
-    active: active || true,
-    cpr: cpr || null,
-    mobile_number: mobile_number || null,
-    gender,
-    accent: accent || null,
-    birthdate: birthdate || null,
-  };
+	req.staff = {
+		teaching_rate,
+		active: active || true,
+		cpr: cpr || null,
+		mobile_number: mobile_number || null,
+		gender,
+		accent: accent || null,
+		birthdate: birthdate || null,
+	};
 
-  next();
+	next();
 });
 
 const validateEditStaff = catchAsync(async (req, res, next) => {
-  const {
-    // username,
-    email,
-    name,
-    short_name,
-    cpr,
-    mobile_number,
-    accent,
-    gender,
-    birthdate,
-    teaching_rate,
-    admin,
-    active,
-  } = req.body;
-  if (
-    admin === null ||
-    admin === undefined ||
-    // !username ||
-    !email ||
-    !name ||
-    !gender
-  ) {
-    return next(new AppError('Wrong Body', 400));
-  }
-  req.user = {
-    user_type: admin ? 'admin' : 'staff',
-    // username,
-    email,
-    name,
-    short_name: short_name || null,
-  };
+	const {
+		// username,
+		email,
+		name,
+		// short_name,
+		cpr,
+		mobile_number,
+		accent,
+		gender,
+		birthdate,
+		teaching_rate,
+		admin,
+		active,
+	} = req.body;
+	if (
+		admin === null ||
+		admin === undefined ||
+		// !username ||
+		!email ||
+		!name ||
+		!gender
+	) {
+		return next(new AppError('Wrong Body', 400));
+	}
+	req.user = {
+		user_type: admin ? 'admin' : 'staff',
+		// username,
+		email,
+		name,
+		short_name: short_name || null,
+	};
 
-  req.staff = {
-    teaching_rate,
-    cpr: cpr || null,
-    mobile_number: mobile_number || null,
-    gender,
-    accent: accent || null,
-    birthdate: birthdate || null,
-    active,
-  };
+	req.staff = {
+		teaching_rate,
+		cpr: cpr || null,
+		mobile_number: mobile_number || null,
+		gender,
+		accent: accent || null,
+		birthdate: birthdate || null,
+		active,
+	};
 
-  const userByEmail = await User.findBy('email', email);
-  // const userByUsername = await User.findBy('username', username);
+	const userByEmail = await User.findBy('email', email);
+	// const userByUsername = await User.findBy('username', username);
 
-  if (userByEmail && userByEmail.user_id !== req.staffUser.user_id) {
-    return next(new AppError('User with that email already exists', 401));
-  }
-  // if (userByUsername && userByUsername.user_id !== req.staffUser.user_id) {
-  //   return next(new AppError('User with that username already exists', 401));
-  // }
+	if (userByEmail && userByEmail.user_id !== req.staffUser.user_id) {
+		return next(new AppError('User with that email already exists', 401));
+	}
+	// if (userByUsername && userByUsername.user_id !== req.staffUser.user_id) {
+	//   return next(new AppError('User with that username already exists', 401));
+	// }
 
-  if (cpr) {
-    const staffByCPR = await Staff.findByCPR(cpr);
-    if (staffByCPR && staffByCPR.staff_id !== req.staffUser.staff_id) {
-      return next(
-        new AppError('Staff member with that CPR already exists', 401)
-      );
-    }
-  }
-  next();
+	if (cpr) {
+		const staffByCPR = await Staff.findByCPR(cpr);
+		if (staffByCPR && staffByCPR.staff_id !== req.staffUser.staff_id) {
+			return next(
+				new AppError('Staff member with that CPR already exists', 401)
+			);
+		}
+	}
+	next();
 });
 
 const validateStaffID = (req, res, next) => {
-  const staffID = +req.params.staffID;
-  if (isNaN(staffID)) {
-    return next(new AppError('Please enter a valid ID', 401));
-  }
-  req.staffID = staffID;
-  next();
+	const staffID = +req.params.staffID;
+	if (isNaN(staffID)) {
+		return next(new AppError('Please enter a valid ID', 401));
+	}
+	req.staffID = staffID;
+	next();
 };
 
 const checkIfStaffExistsByID = catchAsync(async (req, res, next) => {
-  const staff = await Staff.findByID(req.staffID);
-  if (!staff) {
-    next(new AppError('Staff with that ID does not exist', 406));
-    return;
-  }
-  req.staffUser = staff;
-  next();
+	const staff = await Staff.findByID(req.staffID);
+	if (!staff) {
+		next(new AppError('Staff with that ID does not exist', 406));
+		return;
+	}
+	req.staffUser = staff;
+	next();
 });
 
 module.exports = {
-  validateCreateStaff,
-  validateStaffID,
-  validateEditStaff,
-  checkIfStaffExistsByID,
+	validateCreateStaff,
+	validateStaffID,
+	validateEditStaff,
+	checkIfStaffExistsByID,
 };
